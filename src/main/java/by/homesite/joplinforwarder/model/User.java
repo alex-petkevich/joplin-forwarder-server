69296a20caa84b69d43ca28fa998f47dbdc27e6f
Aliaksandr Piatkevich <aliaksandr.piatkevich@rbi-group-it.com>
2023-11-22 17:32:50 +0100

    integration with webdav

--- src/main/java/by/homesite/joplinforwarder/model/User.java
+++ src/main/java/by/homesite/joplinforwarder/model/User.java
@@ -81,9 +81,9 @@ public class User
 			inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
-	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
 	private List<Settings> settingsList;
 
-	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
 	private List<Rule> rules;
 }
