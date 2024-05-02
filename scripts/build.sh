52f8c2cb1cc38e8e6585a590eaed9c0ea55a3bec
Aliaksandr Piatkevich <aliaksandr.piatkevich@rbi-group-it.com>
2024-05-02 21:51:12 +0200

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-chown joplinforwarder:joplinforwarder ./target/joplinforwarder-1.0.0-SNAPSHOT.jar
-chmod u+x ./target/joplinforwarder-1.0.0-SNAPSHOT.jar
+chown joplinforwarder:joplinforwarder ./target/server-1.0.0-SNAPSHOT.jar
+chmod u+x ./target/server-1.0.0-SNAPSHOT.jar
 systemctl stop joplinforwarder
 sleep 3
-cp ./target/joplinforwarder-1.0.0-SNAPSHOT.jar /mnt/vol1/www/joplin/joplinforwarder.jar
+cp ./target/server-1.0.0-SNAPSHOT.jar /mnt/vol1/www/joplin/joplinforwarder.jar
 chown joplinforwarder:joplinforwarder /mnt/vol1/www/joplin/joplinforwarder.jar
 systemctl start joplinforwarder
